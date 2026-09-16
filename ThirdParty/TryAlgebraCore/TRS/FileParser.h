b5c2809aaa7a3ffd84a8e5cabc4d7d1c455141c8
Aspiotis.GO <Aspiotis.GO@W1203-IT>
2026-09-16 19:53:33 +0300

    progress

---
+++ ThirdParty/TryAlgebraCore/TRS/FileParser.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "PatternMatchingHelpers.h"
+
+namespace TryAlgebraCore::Trs
+{
+	class FileParser
+	{
+	public:
+
+	private:
+		std::vector<std::vector<std::unique_ptr<TermIntermediate>>>	m_identities;
+	};
+
+}
